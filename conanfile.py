1d71a47e2366eb598e030e1fa32a74eddaa94e86
kosterinid <42376166+kosterinid@users.noreply.github.com>
2018-11-28 10:45:18 +0300

    Conan support (#3)
    * add conanfile
    
    * rename conanfile
    
    * Delete conafile.py
    
    * add include dir

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, CMake, tools
+
+class CuqConan(ConanFile):
+    name = "cuq"
+    url = "https://github.com/PianeRamso/cuq"
+    description = "CUDA multi-GPU concurrent tasks queue"
+    settings = "os", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources="*"
+
+    def build(self):
+        cmake = CMake(self)
+        self.run("cmake . ")
+        self.run("make -j8")
+
+    def package(self):
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["cuq"]
